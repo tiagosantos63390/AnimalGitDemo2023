e5596ee851e757c4dad2519c89ed62d4ecdc0a84
tiagosantos63390 <tdn.santos@campus.fct.unl.pt>
2023-09-22 12:05:22 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Tiago Santos, n. 63390
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Animal git demo");
+    }
+}
